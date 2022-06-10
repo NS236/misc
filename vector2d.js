fb43d0b242f0e2bf8983f7770d51ddfdf90243a2
NS236 <105520205+NS236@users.noreply.github.com>
2022-06-10 18:35:47 -0400

    Update vector2d.js

--- vector2d.js
+++ vector2d.js
@@ -1,137 +1,136 @@
-class Vector2D {
+function Vector2D(x, y) {
   //default is zero vector
-  constructor(x = 0, y = 0) {
+  if(x && y) {
     this._x = x;
     this._y = y;
-  }
-
-  /**Static Methods**/
-  // returns a new zero vector (Vector2D)
-  static zero() {
-    return new Vector2D(0, 0);
-  }
-  // returns a copy of given vector (Vector2D)
-  static copy(v) {
-    return new Vector2D(v.x, v.y);
-  }
-  // checks if two vectors are equal (boolean)
-  static eq(a, b) {
-    return a.x == b.x && a.y == b.y;
-  }
-  // normalizes given vector (Vector2D)
-  static norm(v) {
-    let r = v.mag;
-    return (r != 0) ? new Vector2D(v.x / r, v.y / r) : new Vector2D(0, 0);
-  }
-  // adds two vectors (Vector2D)
-  static add(a, b) {
-    return new Vector2D(a.x + b.x, a.y + b.y);
-  }
-  // subtracts two vectors (Vector2D)
-  static sub(a, b) {
-    return new Vector2D(a.x - b.x, a.y - b.y);
-  }
-  // finds the dot product of two vectors (number)
-  static dot(a, b) {
-    return a.x * b.x + a.y * b.y;
-  }
-  // finds the cross product of two vectors (number)
-  static cross(a, b) {
-    return a.x * b.y - a.y * b.x;
-  }
-  // multiplies a given vector by a scalar (Vector2D)
-  static scale(v, k) {
-    return new Vector2D(v.x * k, v.y * k);
+  } else {
+    this._x = 0;
+    this._y = 0;
   }
 
   /**Accessing Values**/
   // get x coordinate
-  get x() {
-    return this._x;
-  }
+  this.x = this._x;
   // get y coordinate
-  get y() {
-    return this._y;
-  }
+  this.y = this._y;
   // get magnitude/length
-  get mag() {
+  this.getMag = function() {
     return Math.sqrt(this._x**2 + this._y**2);
-  }
+  };
   // get direction/angle
-  get dir() {
+  this.getDir = function() {
     return Math.atan2(this._y, this._x);
-  }
+  };
 
   /**Setting Values**/
   // set x coordinate
-  setX(x) {
+  this.setX = function(x) {
     this._x = x;
-  }
+  };
   //set y coordinate
-  setY(y) {
+  this.setY = function(y) {
     this._y = y;
-  }
+  };
   // set cartesian coordinates (x and y)
-  setCoords(x, y) {
+  this.setCoords = function(x, y) {
     this._x = x;
     this._y = y;
-  }
+  };
   // set magnitude
-  setMag(r) {
-    let θ = this.dir;
+  this.setMag = function(r) {
+    var θ = this.getDir();
     this._x = r * Math.cos(θ);
     this._y = r * Math.sin(θ);
-  }
+  };
   // set direction
-  setDir(θ) {
-    let r = this.mag;
+  this.setDir = function(θ) {
+    var r = this.getMag();
     this._x = r * Math.cos(θ);
     this._y = r * Math.sin(θ);
-  }
+  };
   // set polar coordinates (magnitude and direction/r and θ)
-  setPolar(r, θ) {
+  this.setPolar = function(r, θ) {
     this._x = r * Math.cos(θ);
     this._y = r * Math.sin(θ);
-  }
+  };
 
   /**Instance Vector Operations**/
   // add another vector to instance
-  add(v) {
+  this.add = function(v) {
     this._x += v.x;
     this._y += v.y;
-  }
+  };
   // subtract another vector from instance
-  sub(v) {
+  this.sub = function(v) {
     this._x -= v.x;
     this._y -= v.y;
-  }
+  };
   // multiply instance by scalar
-  scale(k) {
+  this.scale = function(k) {
     this._x *= k;
     this._y *= k;
-  }
+  };
   // normalize instance
-  norm() {
-    let r = this.mag;
-    this._x = (r != 0) ? this.x / r : 0;
-    this._y = (r != 0) ? this.y / r : 0;
-  }
+  this.norm = function() {
+    var r = this.getMag;
+    this._x = (r !== 0) ? this.x / r : 0;
+    this._y = (r !== 0) ? this.y / r : 0;
+  };
 
   /**Output**/
   // get array of either polar or cartesian coordinates 
-  getArray(polar = false) {
-    return !polar ? [this._x, this._y] : [this.mag, this.dir];
-  }
+  this.getArray = function(polar) {
+    return !polar ? [this._x, this._y] : [this.getMag, this.getDir()];
+  };
   // get object of either polar or cartesian coordinates
-  getObject(polar = false) {
-    return !polar ? {x: this._x, y: this._y} : {r: this.mag, θ: this.dir};
-  }
+  this.getObject = function(polar) {
+    return !polar ? {x: this._x, y: this._y} : {r: this.getMag, θ: this.getDir()};
+  };
   // get string of either polar or cartesian coordinates separated by a space
-  getString(polar = false) {
-    return !polar ? (this._x + " " + this._y) : (this.mag + " " + this.dir);
-  }
+  this.getString = function(polar) {
+    return !polar ? (this._x + " " + this._y) : (this.getMag + " " + this.getDir());
+  };
   // get copy of instance
-  copy() {
+  this.copy = function() {
     return new Vector2D(this.x, this.y);
-  }
+  };
 }
+
+/**Static Methods**/
+// returns a new zero vector (Vector2D)
+Vector2D.zero = function() {
+  return new Vector2D(0, 0);
+};
+// returns a copy of given vector (Vector2D)
+Vector2D.copy = function(v) {
+  return new Vector2D(v.x, v.y);
+};
+// checks if two vectors are equal (boolean)
+Vector2D.eq = function(a, b) {
+  return a.x === b.x && a.y === b.y;
+};
+// normalizes given vector (Vector2D)
+Vector2D.norm = function(v) {
+  var r = v.mag;
+  return (r !== 0) ? new Vector2D(v.x / r, v.y / r) : new Vector2D(0, 0);
+};
+// adds two vectors (Vector2D)
+Vector2D.add = function(a, b) {
+  return new Vector2D(a.x + b.x, a.y + b.y);
+};
+// subtracts two vectors (Vector2D)
+Vector2D.sub = function(a, b) {
+  return new Vector2D(a.x - b.x, a.y - b.y);
+};
+// finds the dot product of two vectors (number)
+Vector2D.dot = function(a, b) {
+  return a.x * b.x + a.y * b.y;
+};
+// finds the cross product of two vectors (number)
+Vector2D.cross = function(a, b) {
+  return a.x * b.y - a.y * b.x;
+};
+// multiplies a given vector by a scalar (Vector2D)
+Vector2D.scale = function(v, k) {
+  return new Vector2D(v.x * k, v.y * k);
+};
